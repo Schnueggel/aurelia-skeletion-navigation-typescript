13fde8146fc41c951bac898c3a75694c235dc833
Christian Steinmann <christian.steinmann77@gmail.com>
2015-09-12 18:00:36 +0200

    Move code from welcome controller into react component

--- src/react-components/UserForm.tsx
+++ src/react-components/UserForm.tsx
@@ -1,32 +1,58 @@
 /// <reference path="../../typings/react/react.d.ts"/>
-import React from 'react';
+import * as React from 'react';
 
 export class UserFormProps {
   public firstname:string;
   public lastname:string;
-  public fullname: string;
-  public submit: Function = () => {};
+  public submit: React.FormEventHandler = () => {};
 }
 
+export class UserFormState {
+  firstname:string;
+  lastname:string;
+}
 export default class UserFormComponent extends React.Component<UserFormProps, any> {
-
+  state: any = {
+    firstname: '',
+    lastname: ''
+  };
   constructor(props:UserFormProps) {
     super(props);
+    this.state.firstname = this.props.firstname;
+    this.state.lastname = this.props.lastname;
   }
+
+ /**
+  * Handles changes of firstname input field
+  **/
+  handleFirstnameChange(event: React.FormEvent) {
+    this.setState({
+      firstname: event.target.value
+    });
+  }
+  /**
+   * Handles changes of lastname input field
+   **/
+  handleLastnameChange(event: React.FormEvent) {
+    this.setState({
+      lastname: event.target.value
+    });
+  }
+
   render() {
     return (
       <form role="form" onSubmit={this.props.submit}>
-        <div className="form-group">
-          <label htmlFor="fn">First Name</label>
-          <input type="text" value={this.props.firstname} className="form-control" id="fn" placeholder="firstname"/>
+        <div className="form-group">{console.log("hund")}
+          <label htmlFor="fn">Firstname</label>
+          <input type="text" value={this.state.firstname} className="form-control" id="fn" placeholder="Firstname" onChange={this.handleFirstnameChange.bind(this)}/>
         </div>
         <div className="form-group">
-          <label htmlFor="ln">Last Name</label>
-          <input type="text" value={this.props.lastname} className="form-control" id="ln" placeholder="lastname"/>
+          <label htmlFor="ln">Lastname</label>
+          <input type="text" value={this.state.lastname} className="form-control" id="ln" placeholder="Lastname" onChange={this.handleLastnameChange.bind(this)}/>
         </div>
         <div className="form-group">
-          <label>Full Name</label>
-          <p className="help-block">{this.props.fullname}</p>
+          <label>FullName</label>
+          <p className="help-block">{this.state.firstname} {this.state.lastname}</p>
         </div>
         <button type="submit" className="btn btn-default" label="Submit">Submit</button>
       </form>
--- src/user-form.tsx
+++ src/user-form.tsx
@@ -1,30 +1,33 @@
-import React from 'react';
+import * as React from 'react';
+
 import {inject, noView, bindable, customElement} from 'aurelia-framework';
 import UserFormComponent from './react-components/UserForm';
+import { UserFormProps } from './react-components/UserForm';
 
 @noView()
 @inject(Element)
 export class UserForm {
-
+  userFormProps: UserFormProps;
   element: Element;
 
   constructor(element) {
     this.element = element;
+    this.userFormProps = new UserFormProps();
+    this.userFormProps.firstname = 'John';
+    this.userFormProps.lastname = 'Doe';
+    this.userFormProps.submit = this.submit.bind(this);
   }
 
   render() {
-    React.render(<UserFormComponent submit={this.submit}/>, this.element);
+    React.render(<UserFormComponent {...this.userFormProps}/>, this.element);
   }
 
   bind() {
     this.render();
   }
 
-  submit(firstname : string, lastname: string) {
-    console.log(firstname);
-  }
-
-  disabledChanged() {
-    this.render();
+  submit(event: __React.FormEvent) {
+    this.userFormProps.firstname = 'Johfffn';
+    console.log(event);
   }
 }
